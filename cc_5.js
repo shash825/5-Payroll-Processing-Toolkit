127da96b43e35c58d9cba229ec68603d04e32993
Shashwat <ss44@usf.edu>
2026-02-22 15:57:46 -0500

    added loop function and payroll output

--- cc_5.js
+++ cc_5.js
@@ -50,3 +50,14 @@ function processPayroll(employee) {
         netPay: netPay
     };
 }
+
+//step 7
+employees.forEach(employee => {
+    const payroll = processPayroll(employee);
+    
+    console.log(`Employee: ${payroll.name}`);
+    console.log(`Base Pay: $${payroll.basePay.toFixed(2)}`);
+    console.log(`Overtime Pay: $${payroll.overtimePay.toFixed(2)}`);
+    console.log(`Gross Pay: $${payroll.grossPay.toFixed(2)}`);
+    console.log(`Net Pay: $${payroll.netPay.toFixed(2)}`);
+});
