e021a4f696d99db09d777ca1ab29d3e5347b552e
Shashwat <ss44@usf.edu>
2026-02-22 15:36:03 -0500

    made array with employees

---
+++ cc_5.js
@@ -0,0 +1,18 @@
+const employees = [
+    {
+        name: "Aiden",
+        hoursWorked: 25,
+        hourlyRate: 15
+    },
+    {
+        name: "Rj",
+        hoursWorked: 40,
+        hourlyRate: 20
+    }, 
+    {
+        name: "Alex",
+        hoursWorked: 35,
+        hourlyRate: 18
+    }
+];
+
